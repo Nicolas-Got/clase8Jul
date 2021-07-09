6e4092b33762655bf940b84f5cec84389c795ca1
Nicolas-Got <legolasnicolas99@hotmail.com>
2021-07-08 20:11:36 -0500

    Suma

--- src/App.java
+++ src/App.java
@@ -2,6 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Perro");
+        int n= 10;
+        int m =30;
+        int r = n+m;
+        System.out.println(r);
+        
 
     }
 }
